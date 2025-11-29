01e1272c4ba83058fef957aff395420ecbb34375
elevantcoding <meli.of.nj@gmail.com>
2025-11-29 14:50:16 -0500

    Add files via upload
    Use LEAD() to derive an end date from sequential start dates.

---
+++ use-lead-to-derive-end-date.sql
@@ -0,0 +1,27 @@
+-- use LEAD to derive an End Date from Sequential Start Dates
+
+-----------------------------------------------------------------------------
+--| Rate ID | EmployeeID | PositionID | HourlyRate | StartDate  | End Date  |
+-----------------------------------------------------------------------------
+--| 1	    | 1	         | 20	      | 20.00	   | 2011-05-01 | 2013-05-26|
+--| 2	    | 1	         | 20	      | 21.00	   | 2013-05-27 | 2015-03-22|
+--| 3	    | 1	         | 20	      | 22.50	   | 2015-03-23 | 2050-12-31|
+--| 4	    | 1	         | 45	      | 22.00	   | 2011-05-01 | 2013-05-26|
+--| 5	    | 1	         | 45	      | 24.00	   | 2013-05-27 | 2015-03-22|
+--| 6	    | 1	         | 45	      | 26.50	   | 2015-03-23 | 2050-12-31|
+
+SELECT
+r.RateID,
+r.EmployeeID,
+r.PositionID,
+r.HourlyRate,
+r.StartDate,
+
+CASE 
+    WHEN LEAD(r.StartDate) OVER (PARTITION BY r.EmployeeID, r.PositionID ORDER BY r.StartDate) IS NOT NULL THEN 
+        DATEADD(DAY, -1, LEAD(r.StartDate) OVER (PARTITION BY r.EmployeeID, r.PositionID ORDER BY r.StartDate))        
+    ELSE 
+        '12/31/2050'
+END AS EndDate
+FROM elevant.RateTable r
+ORDER BY r.EmployeeID, r.PositionID
