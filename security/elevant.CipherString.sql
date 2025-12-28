db0de942d7e5364c5efa166968c33a62c570ea83
elevantcoding <meli.of.nj@gmail.com>
2025-12-28 17:18:03 -0500

    Update elevant.CipherString.sql

--- security/elevant.CipherString.sql
+++ security/elevant.CipherString.sql
@@ -1,28 +1,23 @@
-USE [SAMPLE]
-GO
-
-/****** Object:  UserDefinedFunction [elevant].[CipherString]    Script Date: 12/28/2025 1:57:53 PM ******/
+/****** Object:  UserDefinedFunction [elevant].[CipherString]    Script Date: 12/28/2025 5:16:25 PM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-
-
 -- perform obfuscation of a string value using random values provided to the function
 -- the string will contain the key for deobfuscation and can be deobfuscated by 
 -- DecipherString
 
-ALTER FUNCTION [elevant].[CipherString](@string varchar(128), @vals nvarchar(6), @randval tinyint, @ciph nvarchar(10), @padding nvarchar(128))
-RETURNS nvarchar(256)
+ALTER FUNCTION [elevant].[CipherString](@string varchar(128), @vals varchar(6), @randval tinyint, @ciph varchar(10), @padding varchar(128))
+RETURNS varchar(256)
 AS
 BEGIN
 	DECLARE @strLen int;
 	DECLARE @i int;
 	DECLARE @loops int;
 	DECLARE @v int;
-	DECLARE @altervals nvarchar(6);
+	DECLARE @altervals varchar(6);
 	DECLARE @loopcount int;
 	DECLARE @char varchar(1);
 	DECLARE @getasc int;
@@ -30,7 +25,7 @@ BEGIN
 	DECLARE @addasc int;
 	DECLARE @addchar varchar(1);
 	DECLARE @prefix varchar(21);
-	DECLARE @key nvarchar(10);
+	DECLARE @key varchar(10);
 	DECLARE @c int;
 	DECLARE @ciphprefix varchar(21);
 	DECLARE @prefixlen int;
@@ -40,8 +35,8 @@ BEGIN
 	DECLARE @s int;
 	DECLARE @p int;
 	DECLARE @paddedstring varchar(128);
-	DECLARE @hexstring nvarchar(256);
-	DECLARE @hexvalue nvarchar(2);
+	DECLARE @hexstring varchar(256);
+	DECLARE @hexvalue varchar(2);
 
 	-- handle input errors
 	SET @string = TRIM(@string);
@@ -76,14 +71,13 @@ BEGIN
 			SET @getasc = ASCII(@char);
 			SET @getval = CAST(SUBSTRING(@altervals, @v, 1) AS INT);
 			SET @addasc = @getasc ^ @getval;
-			SET @addchar = CHAR(@addasc);
-			
-			SET @string = elevant.ReplaceCharAtIndex(@string, @i, @addchar);
+			SET @addchar = CHAR(@addasc);			
+			SET @string = STUFF(@string, CHARINDEX(@char, @string, @i),1, @addchar)
 			SET @loopcount = @loopcount + 1;
 		END
 
 	-- create prefix / key
-	SET @prefix = CONCAT(@altervals, CAST(@randval AS nvarchar(1)), CAST(@v AS nvarchar(1)), FORMAT(@strLen,'000'))
+	SET @prefix = CONCAT(@altervals, CAST(@randval AS varchar(1)), CAST(@v AS varchar(1)), FORMAT(@strLen,'000'))
 
 	-- use @ciph to cipher the prefix
 	SET @key = N'0123456789';
@@ -155,5 +149,6 @@ BEGIN
 	RETURN @hexstring
 END
 GO
+
 
 
